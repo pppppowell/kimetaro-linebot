08046674a29d759495269b0ae5e98d790c92bb7a
pppppowell <aiueonigiri1121@gmail.com>
2019-10-30 09:18:21 +0900

    type miss

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, webhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
