efdb0f9ba6ae3050afdb0d0660b64b57a4f0cf22
pppppowell <aiueonigiri1121@gmail.com>
2019-10-31 15:47:08 +0900

    response

--- account_response.py
+++ account_response.py
@@ -22,10 +22,10 @@ class Response:
     }
 
     def nameResponse(self):
-        rand=random.randrange(1,len(self.name)+1)
-        return self.name[rand]
+        # rand=random.randrange(1,len(self.name)+1)
+        return self.name[1]
 
     def judgeResponse(self):
-        rand = random.randrange(1, len(self.judge)+1)
-        return self.judge[rand]
+        # rand = random.randrange(1, len(self.judge)+1)
+        return self.judge[1]
     
